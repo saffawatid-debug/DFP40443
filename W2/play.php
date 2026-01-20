58633f44c9e23912a47e609c3e7a9de6f80e9715
Dayang Nur Saffawati <saffawatid@gmail.com>
2026-01-20 15:17:38 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -1,4 +1,8 @@
 <?php
-echo "Hello World";
-echo 'This is a full stack class';
+$name = "dayang";
+echo "Hello World $name";
+echo 'This is a full stack class'.$name;
+echo "<h1> My name is Dayang";
+echo "<h2> My PA name is Miss Athirah";
+echo "<h3> My department name is JTMK";
 ?>
